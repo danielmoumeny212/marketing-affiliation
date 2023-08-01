29fe0f2426b93f664b250cddaa40dba3933b762e
Manasse Daniel <danidkm242@gmail.com>
2023-08-01 23:21:18 +0100

    get links handler

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -1,13 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { LinkService } from './link.service';
 
-@Controller('link')
+@Controller()
 export class LinkController {
   
   constructor(
     private linkServices: LinkService
   ){
-    
+
+  }
+
+  @Get("admin/users/:id/links")
+  async all(@Param("id", ParseIntPipe) id: number ){
+      return this.linkServices.find({
+        user: id
+      })
   }
 
 }
