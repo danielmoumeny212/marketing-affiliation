a1566a1b87e21cb597e5db999e431b8c7ea575ac
Manasse Daniel <danidkm242@gmail.com>
2023-08-20 00:30:06 +0100

    searching & sorting

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,77 +1,114 @@
 import { CreateProductDto, UpdateProductDto } from './dtos';
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put , UseGuards, UseInterceptors, Inject} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseGuards,
+  UseInterceptors,
+  Inject,
+  Req,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { AuthGuard } from 'src/auth/auth.guard';
-import { CACHE_MANAGER, CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
+import {
+  CACHE_MANAGER,
+  CacheInterceptor,
+  CacheKey,
+  CacheTTL,
+} from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
 import { EventEmitter2 } from '@nestjs/event-emitter';
+import { Request } from 'express';
 
-
-@UseGuards(AuthGuard)
+// @UseGuards(AuthGuard)
 @Controller()
 export class ProductController {
   constructor(
     private productService: ProductService,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
-    private eventEmitter: EventEmitter2
-    ){}
+    private eventEmitter: EventEmitter2,
+  ) {}
 
-  @Get("admin/products")
-  async allProducts(){
+  @Get('admin/products')
+  async allProducts() {
     return await this.productService.find({});
   }
 
-  @Post("admin/products")
-  async create(@Body() body: CreateProductDto){ 
-    const product =  await this.productService.save(body);
-    this.eventEmitter.emit("product_updated");
-   return product;
-  } 
-
-  @Get("admin/products/:id")
-  async findOne(@Param("id", ParseIntPipe) id: number){
-     return await this.productService.findOne({id})
+  @Post('admin/products')
+  async create(@Body() body: CreateProductDto) {
+    const product = await this.productService.save(body);
+    this.eventEmitter.emit('product_updated');
+    return product;
   }
 
-  @Put("admin/products/:id")
-  async update(@Param("id", ParseIntPipe) id: number, 
-             @Body() product: UpdateProductDto){
-     await this.productService.update(id, product); 
-
-     const updatedProduct =await this.productService.findOne({ where: {id}}); 
-     this.eventEmitter.emit("product_updated");
-
-
-
-     return updatedProduct;
+  @Get('admin/products/:id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.productService.findOne({ id });
   }
 
-  @Delete("admin/products/:id")
-  async remove (@Param("id", ParseIntPipe) id: number){
-      await this.productService.delete(id);
-       this.eventEmitter.emit("product_updated");
+  @Put('admin/products/:id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() product: UpdateProductDto,
+  ) {
+    await this.productService.update(id, product);
 
-      return {
-        message: "product deleted successfully"
-      }
+    const updatedProduct = await this.productService.findOne({ where: { id } });
+    this.eventEmitter.emit('product_updated');
+
+    return updatedProduct;
   }
-  @Get("ambassador/products/frontend")
-  @CacheKey("products_frontend")
-  @CacheTTL( 30 * 60)
+
+  @Delete('admin/products/:id')
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    await this.productService.delete(id);
+    this.eventEmitter.emit('product_updated');
+
+    return {
+      message: 'product deleted successfully',
+    };
+  }
+  @Get('ambassador/products/frontend')
+  @CacheKey('products_frontend')
+  @CacheTTL(30 * 60)
   @UseInterceptors(CacheInterceptor)
-  async frontend(){
-    return this.productService.find(); 
+  async frontend() {
+    return this.productService.find();
   }
 
-  @Get("ambassador/products/backend")
-  async backend(){
-    const products = await this.productService.find();
+  @Get('ambassador/products/backend')
+  async backend(@Req() request: Request) {
+    const { sort, search } = request.query;
+    let products = await this.productService.find();
+    if (search) {
+      const s = search.toString().toLowerCase();
+
+      products = products.filter(
+        (p) =>
+          p.name.toLowerCase().indexOf(s) >= 0 ||
+          p.description.toLowerCase().indexOf(s) >= 0,
+      );
+    }
+
+    if (sort === 'asc' || sort === 'desc') {
+      products.sort((a, b) => {
+        const diff = a.price - b.price;
+        if (diff === 0) return 0;
+        const sign = Math.abs(diff) / diff;
+        return sort === 'asc' ? sign : -sign;
+      });
+    }
 
     // let  products = await this.cacheManager.get("products_backend");
     // if(!products){
     //    products = await this.productService.find();
     //    await this.cacheManager.set("products_backend", products, 1000);
     // }
-    return products; 
+    return products;
   }
 }
