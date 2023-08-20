4fdf43d9856154ea8f362c55e51a15d8f67fe0e0
Manasse Daniel <danidkm242@gmail.com>
2023-08-20 21:38:41 +0100

    ambassor stats

--- src/link/link.controller.ts
+++ src/link/link.controller.ts
@@ -12,6 +12,7 @@ import {
 import { Request } from 'express';
 import { LinkService } from './link.service';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { Order } from 'src/order/order';
 
 @Controller()
 export class LinkController {
@@ -34,11 +35,30 @@ export class LinkController {
     const user = await this.authService.getAuthenticatedUser(request);
 
     return this.linkService.save({
-      code: Math.random.toString().substring(6),
+      code: Math.random().toString().substring(6),
       user,
       products: products.map((id) => ({
         id,
       })),
+    });
+  }
+
+  @Get('ambassador/stats')
+  async stats(@Req() request: Request) {
+    const user = this.authService.getAuthenticatedUser(request);
+
+    const links = await this.linkService.find({
+      where: { user },
+      relations: ['orders'],
+    });
+    return links.map((link) => {
+      const completeOrders: Order[] = link.orders.filter((o) => o.complete);
+
+      return {
+        code: link.code,
+        count: completeOrders.length,
+        revenue: completeOrders.reduce((s, o) => s + o.ambassador_revenue, 0),
+      };
     });
   }
 }
